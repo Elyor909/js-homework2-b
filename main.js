2b9fc894e02a0bc1a43a6abafd288fb1ac0dd804
Elyor <nuridinove2001@gmail.com>
2022-12-26 20:43:32 +0500

    jsb

---
+++ main.js
@@ -0,0 +1,20 @@
+var time = +prompt('Введите время');
+
+switch (time) {
+    case 0: case 1: case 2: case 3: case 4: case 5: case 6:
+        alert(time + ' часа ночи');
+        break;
+    case 7: case 8: case 9: case 10:
+        alert(time + ' часов утра');
+        break;
+    case 11: case 12: case 13: case 14: case 15: case 16: case 17:
+        alert(time + ' часа дня');
+        break;
+    case 18: case 19: case 20: case 21: case 22: case 23:
+        alert(time + ' часов вечера');
+        break;
+
+    default:
+        alert('Что-то пошло не так');
+        break;
+}
